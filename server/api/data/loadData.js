f259c742829a912f8aaba24dd69485000c82afec
alexislopez1987 <alexis.lopez87@gmail.com>
2019-10-07 17:41:33 -0300

    seeding items

--- server/api/data/loadData.js
+++ server/api/data/loadData.js
@@ -13,12 +13,32 @@ users.forEach(function (user, index) {
     user.password = hashedPassword;
 });
 
-//let items = JSON.parse(fs.readFileSync(__dirname + '/items.json', 'utf-8'));
+let items = JSON.parse(fs.readFileSync(__dirname + '/items.json', 'utf-8'));
+//const distinctOwners = [...new Set(items.map(x => x.owner))];
 
 exports.loadData = async function loadData() {
     try {
+        let itemsToInsert = [];
+
         console.log("inserting data...");
         await User.insertMany(users);
+
+        const insertedUsers = await User.find({});
+
+        insertedUsers.forEach(function (user, index) {
+
+            let itemsByUser = items.filter(function (el) {
+                return el.owner ===  user.email;
+            });
+
+            itemsByUser.forEach(function (item, index) {
+                item.created = new Date();
+                item.owner = user._id;
+                itemsToInsert.push(item);
+            });
+        });
+
+        await Item.insertMany(itemsToInsert);
         console.log("data inserted");
     } catch (e) {
         console.log("error trying to insert data");

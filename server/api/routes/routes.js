2199b174992342e4ea7a9734d394cbb5f3843b09
Alexis Lopez <alexis.lopez@stgchile.cl>
2019-10-11 15:47:42 -0300

    add swagger

--- server/api/routes/routes.js
+++ server/api/routes/routes.js
@@ -1,10 +1,31 @@
 'use strict';
-var express = require('express')
-var router = express.Router()
+const express = require('express');
+const router = express.Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 const verifyToken = require('./verifyToken');
 
-var itemController = require('../controllers/itemController');
-var userController = require('../controllers/userController');
+const itemController = require('../controllers/itemController');
+const userController = require('../controllers/userController');
+
+const options = {
+    swaggerDefinition: {
+      info: {
+        title: 'Personal Finances API',
+        version: '1.0.0',
+        description: 'Doc for apis',
+        contact: {
+            name: "Alexis Lopez"
+        }
+      },
+    },
+    apis: ['api/routes/routes.js']
+};
+
+const specs = swaggerJsdoc(options);
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(specs));
 
 router.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now())
@@ -30,6 +51,17 @@ router.get('/itemdetail/:id', function (req, res) {
     itemController.item_detail(req, res);
 })
 
+/**
+ * @swagger
+ * /api/v1/item:
+ *    get:
+ *      description: This should return all items
+ *      responses:
+ *        '200':
+ *          description: A successful response
+ *      tags:
+ *       - items
+ */
 router.get('/item', function (req, res) {
     itemController.list_all_items(req, res);
 })
